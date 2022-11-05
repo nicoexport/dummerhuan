986ff4ef4f19f73e043891fb112465362ae69c09
Nico Auerswald <auersw.nico@gmail.com>
2022-11-05 22:27:28 +0100

    implement reactions

--- Assets/Scripts/Combat/CombatManager.cs
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -17,15 +17,13 @@ namespace Dummerhuan.Combat {
         [SerializeField] private FloatReference playerCurrentHealth;
         [SerializeField] private TextBoxUI enemyTextBox;
         [SerializeField] private TextBoxUI playerTextBox;
-
-        [SerializeField] private Effectiveness effi;
-
+        [SerializeField] private PortraitUI enemyPortrait;
+        
         private InsultSO intendedInsult;
         private Coroutine combatCoroutine;
         private IMiniGame currentMiniGame;
         private InsultType intendedInsultType;
         
-
         protected void Awake() {
             if (combatCoroutine != null) {
                 StopCoroutine(combatCoroutine);
@@ -39,16 +37,22 @@ namespace Dummerhuan.Combat {
         private IEnumerator CombatLoop_Co() {
             while (true) {
                 yield return new WaitUntil(()=>intendedInsult);
+                
+                var effect = currentEnemy.Value.effectivenesses[intendedInsultType];
+                
                 yield return playerTextBox.DisplayText_Co("You: " + intendedInsult.Insult, 1f);
-                yield return enemyTextBox.DisplayText_Co("Enemy: ...", 0.5f);
+                yield return enemyTextBox.DisplayText_Co("Enemy: ...", 1f);
+                var reactionSprite = currentEnemy.Value.reactionSprites[(int)effect];
+                enemyPortrait.SetSpriteTempForSeconds(reactionSprite, 1.5f);
                 yield return enemyTextBox.DisplayText_Co("Enemy: " + intendedInsult.Response, 1.5f);
+
                 
                 var miniGamePrefab = currentEnemy.Value.miniGamePrefab;
                 var miniGame = Instantiate(miniGamePrefab, transform.position + miniGameSpawnOffset, 
                     quaternion.identity);
                 miniGame.TryGetComponent(out currentMiniGame);
                 if (currentMiniGame != null) {
-                    currentMiniGame.Setup(effi);
+                    currentMiniGame.Setup(effect);
                     yield return currentMiniGame.Execute();
                 }
                 intendedInsult = null;
--- Assets/Scripts/Combat/EnemySO.cs
+++ Assets/Scripts/Combat/EnemySO.cs
@@ -1,14 +1,21 @@
+using MyBox;
 using Slothsoft.UnityExtensions;
 using UnityEngine;
 
 namespace Dummerhuan.Combat {
     [CreateAssetMenu(menuName = "ScriptableObjects/Enemy", order = 0)]
     public class EnemySO : ScriptableObject {
-        public Sprite portrait;
         public SerializableKeyValuePairs<InsultType, InsultSO[]> possibleInsults = 
             new();
 
+        [Separator]
+        public SerializableKeyValuePairs<InsultType, Effectiveness> effectivenesses;
+        [Separator]
         public GameObject miniGamePrefab;
+
+        [Separator] 
+        public Sprite idleSprite;
+        public Sprite[] reactionSprites;
 
     }
 }
